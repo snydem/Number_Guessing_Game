303d11bae18654b806276e75b3d6e1495a8a04d5
Snydem <cs059588@gmail.com>
2016-12-31 15:57:37 -0500

    Add files via upload

---
+++ number guessing.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#define true 1
+#define false 0
+
+int main(void){
+	int game;
+	float number, guess;
+	number = 42;
+	game = true;
+	printf("I am thinking of a number one through fifty, can you guess it?\n");
+	while (game == true) {
+	scanf("%f", &guess);
+	if (guess > number) {
+		printf("That is too high!\n");
+	}
+	else if (guess < number) {
+		printf("That is too low!\n");
+	}
+	else{
+		printf("YOU GOT IT!!!\n");
+		game == false;
+	};
+	}
+}
